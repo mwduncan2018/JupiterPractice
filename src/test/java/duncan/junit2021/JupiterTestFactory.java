45b44fba03c38b3b3e96461586335a1c5f2ec307
Michael Duncan <mwduncan2018@gmail.com>
2021-04-04 16:24:22 -0400

    updates

--- src/test/java/duncan/junit2021/JupiterTestFactory.java
+++ src/test/java/duncan/junit2021/JupiterTestFactory.java
@@ -8,6 +8,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -17,6 +19,7 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.function.ThrowingConsumer;
 
 import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
@@ -30,11 +33,13 @@ class JupiterTestFactory {
 	private class Pet {
 		public String name;
 		public Integer age;
+		public Supplier<Boolean> isOld = () -> age > 10;
 
 		public Pet(String name, Integer age) {
 			this.name = name;
 			this.age = age;
 		}
+
 	}
 
 	@BeforeEach
@@ -50,10 +55,10 @@ class JupiterTestFactory {
 	@DisplayName("Dynamic Demo")
 	@TestFactory
 	Stream<DynamicTest> dynamicDemo() {
-		return IntStream.range(100, 150).limit(3)
+		return IntStream.range(1000, 9999).limit(3)
 				.mapToObj(x -> dynamicTest("Dyno Demo #" + String.valueOf(x) + "... ", () -> {
 					Pet pet = new Pet("Fluffy" + String.valueOf(x), x);
-					System.out.println(pet.name + " - " + String.valueOf(x));
+					System.out.println(pet.name + " - " + String.valueOf(pet.age));
 					assertEquals(x, pet.age);
 				}));
 	}
@@ -61,14 +66,38 @@ class JupiterTestFactory {
 	@DisplayName("Dyanamic Random Demo")
 	@TestFactory
 	Stream<DynamicTest> dynamicRandomeDemo() {
-		Random rand = new Random();
-		IntStream unlimitedIntStream = rand.ints(0,100000);
-		IntStream limitedIntStream = unlimitedIntStream.limit(5000);
-		return limitedIntStream.mapToObj(x -> dynamicTest("Dyno Random Demo #" + String.valueOf(x), () -> {
-			Pet pet = new Pet("Harry" + String.valueOf(x), x);
-			System.out.println(pet.name + " ? " + String.valueOf(x));
-			assertEquals(x, pet.age);
-		}));
+		return new Random().ints(1000, 9999).limit(5)
+				.mapToObj(x -> dynamicTest("Dyno Random Demo #" + String.valueOf(x), () -> {
+					Pet pet = new Pet("Harry" + String.valueOf(x), x);
+					System.out.println(pet.name + " ? " + String.valueOf(pet.age));
+					assertEquals(x, pet.age);
+				}));
 	}
 
+	@DisplayName("Dynamic Stream Demo")
+	@TestFactory
+	Stream<DynamicTest> dynamicStreamDemo() {
+		return DynamicTest.stream(new Random().ints(1000, 9999).limit(5).iterator(), (x) -> {
+			return "Dyno Stream Demo #" + x;
+		}, (x) -> {
+			Pet pet = new Pet("Cloud" + String.valueOf(x), x);
+			System.out.println(pet.name + " & " + String.valueOf(pet.age));
+			assertEquals(x, pet.age);
+		});
+	}
+
+	@DisplayName("Pets are old for ages greater than 10")
+	@TestFactory
+	Stream<DynamicTest> dynamicStreamPractice() {
+		Iterator<Integer> i = new Random().ints(11, 9999).limit(10).iterator();
+		Function<Integer, String> n = (input) -> {
+			return "Dynamic Stream Practice #" + String.valueOf(input);
+		};
+		ThrowingConsumer<Integer> x = (input) -> {
+			Pet pet = new Pet("Pet" + String.valueOf(input), input);
+			System.out.println(pet.name + " * " + String.valueOf(pet.age));
+			assertTrue(pet.isOld.get());
+		};
+		return DynamicTest.stream(i, n, x);
+	}
 }
